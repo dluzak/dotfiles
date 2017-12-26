95592f0440997cdc0b3894ca81125707560029a1
Karol D <dluzak7@gmail.com>
2017-12-26 17:57:29 +0100

    Added locked monitor power saving.
    When computer is locked, the monitors instead of being black and having
    backlight on, are getting off after few seconds of inactivity.

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -6,7 +6,10 @@ if playerctl status | grep Playing; then
 	playerctl pause
 fi
 
+xset +dpms
+xset dpms 10
 i3lock -n -c 000000
+xset -dpms
 
 # if player was paused before lock: resume it
 if [ $resume = 1 ]; then
